00c50b404c039c5eee5fdf001c7567a3cae0b40a
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-05-13 15:57:33 -0500

    created the spring boot project

---
+++ src/main/java/com/whelmed/demo/WhelmedApplication.java
@@ -0,0 +1,13 @@
+package com.whelmed.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhelmedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhelmedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whelmed/demo/WhelmedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whelmed.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhelmedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

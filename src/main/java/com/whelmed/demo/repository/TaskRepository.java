8434bea7dfa204e305c0e30fd26f4b61d3dc3a87
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-05-16 09:38:39 -0500

    Added the task repository

--- src/main/java/com/whelmed/demo/repository/TaskRepository.java
+++ src/main/java/com/whelmed/demo/repository/TaskRepository.java
@@ -1,10 +1,10 @@
 package com.whelmed.demo.repository;
 
-import com.whelmed.demo.model.Group;
+import com.whelmed.demo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    Group findByName(String groupName);
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Task findByTitle(String titleName);
 }
